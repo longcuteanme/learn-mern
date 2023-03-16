9725e38b51fcdc46bff0ed30086399e75e58befc
Long Trinh <psatrinhlongat48@gmail.com>
2023-03-16 23:52:27 +0700

    feat: create view engine

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const { engine } = require('express-handlebars');
 const connectDB = require("./config/db");
-const home = require("./routes/index")
+const home = require("./routes/index");
 
 connectDB();
 
 const app = express();
 const port = 3000;
 
-app.use(express.json);
-app.use("/", home)
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set("views", "./views");
+
+// app.use(express.json);
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// app.use("/", home);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-// router.use((req, res, next) => {
-//   console.log("Time: ", Date.now());
-//   next();
-// });
+const Post = require("../models/Post")
+
+router.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
 // define the home page route
 router.get("/", (req, res) => {
   res.send("Home page");
 });
 // define the about route
+router.get("/about", (req, res) => {
+  res.send("about");
+});
+
 router.get("/about", (req, res) => {
   res.send("about");
 });
