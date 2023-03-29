1f41cf0a6e18dc8a64e81c011ea2f20857c051d2
Nguyen Anh <nguyenanh@mor.com.vn>
2023-03-29 17:42:10 +0700

    feat: add screen home

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const home = require("./routes/index")
+const home = require("./routes/index");
+const { engine } = require("express-handlebars");
 
 connectDB();
 
 const app = express();
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
 const port = 3000;
 
-app.use(express.json);
-app.use("/", home)
+// app.use(express.json);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
